e2d2b3448539b54b671a850b8a22a09e1fe0ca70
jayshree200 <jayshree24@navgurukul.org>
2024-11-01 12:48:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+
+//let marks = [ 23, 48, 94,78,60];
+//console.log(marks);
+//console.log(marks, length); //property
+//let heros = ["ironmen", "thor", "shaktiman"];
+//console.log(heros[0]);
+//console.log(heros[1]);
+
+//let items = [250, 645, 300,900,50];
+//let idx = 0;
+//for (let val of items){
+//    console.log (`value at index ${idx} = ${val}`);
+//    idx++;
+//}
+
+//let items = [250, 645, 300, 900, 50];
+//for (let i = 0; i < items.length; i++) {
+  //  let offer = items[i] / 10;
+  //  items[i] -= offer;
+//}
+
+//console.log(items);
+
+
+//let marvelHeroes = ["thor", "spiderman", "ironman"];
+//let dcHeroes = ["superman", "batman"];
+
+//let heroes = marvelHeroes.concat(dcHeroes);
+//console.log(heroes);
+
+let arr = [1, 2, 3, 4, 5, 6, 7];
+
+//arr.splice(2, 101, 102);
+//add Element
+arr.splice(2, 0, 101);
